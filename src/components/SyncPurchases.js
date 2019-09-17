d440c35115cc0d53a05815a7c16d4e619ef4be86
Thaynã B. Moretti <moretty@gmail.com>
2019-09-17 10:39:14 -0300

    Fix indentation

--- src/components/SyncPurchases.js
+++ src/components/SyncPurchases.js
@@ -2,41 +2,47 @@ import axios from 'axios'
 import config from '../config'
 
 const SyncPurchases = {
-    job: false,
-    startJob: () => {
-        if (!SyncPurchases.job) {
-            setInterval(SyncPurchases.syncData, 20000);
-            SyncPurchases.job = true;
-        }
-    },
-    purchaseKey: () => {
-        return `pcs::${new Date().getTime()}`
-    },
-    storePurchase: (purchase) => {
-        localStorage.setItem(SyncPurchases.purchaseKey(), JSON.stringify(purchase));
-    },
-    syncData: async () => {
-        for (var key in localStorage) {
-            if (key.indexOf('pcs::') === 0) {
-                var purchase = JSON.parse(localStorage.getItem(key))
+  job: false,
 
-                try {
-                    await axios({
-                        method: 'post',
-                        url: config.googleScriptURL + '?action=create_transaction',
-                        data: purchase,
-                        headers: {
-                            'Content-Type': 'text/plain',
-                        }
-                    })
-
-                    localStorage.removeItem(key);
-                } catch (error) {
-                    break;
-                }
-            }
-        }
+  startJob: () => {
+    if (!SyncPurchases.job) {
+      setInterval(SyncPurchases.syncData, 20000)
+      SyncPurchases.job = true
     }
-};
+  },
 
-export default SyncPurchases;
+  purchaseKey: () => {
+    return `pcs::${new Date().getTime()}`
+  },
+
+  storePurchase: (purchase) => {
+    localStorage.setItem(SyncPurchases.purchaseKey(), JSON.stringify(purchase))
+  },
+
+  syncData: async () => {
+    for (let key in localStorage) {
+      if (key.indexOf('pcs::') === 0) {
+        try {
+          let purchase = JSON.parse(localStorage.getItem(key))
+
+          await SyncPurchases.sendPurchase(purchase)
+
+          localStorage.removeItem(key)
+        } catch (error) {
+          break
+        }
+      }
+    }
+  },
+
+  sendPurchase: async (purchase) => {
+    await axios({
+      method: 'post',
+      url: config.googleScriptURL + '?action=create_transaction',
+      data: purchase,
+      headers: { 'Content-Type': 'text/plain' }
+    })
+  }
+}
+
+export default SyncPurchases
